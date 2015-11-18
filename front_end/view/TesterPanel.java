0ef1356d81eae0a58a1d0028f699af6191d3742d
Kate Hutchinson <normal@Katherine-Hutchinsons-MacBook-Pro-2.local>
2015-11-18 05:34:46 -0500

    Fixing view stuffs

--- front_end/view/TesterPanel.java
+++ front_end/view/TesterPanel.java
@@ -7,6 +7,9 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 import model.*;
 
@@ -58,22 +61,117 @@ class CardPanel extends JPanel {
 
 
 public class TesterPanel extends JPanel{
-     ResponsePanel response;
-     CardPanel cardsides;
      
     
-    public TesterPanel(FlashCard card, String str) {
+    public TesterPanel(FlashCard card, String response, Boolean side1wasShown) {
+    
+        this.setLayout(new BorderLayout());
         
-        GridLayout grid = new GridLayout();
-        grid.setColumns(2);
-        grid.setRows(1);
-        this.setLayout(grid);
+        CardPanel cardPanel;
+        if(side1wasShown) {
+            cardPanel = new CardPanel(card.getSide1(), card.getSide2());
+        }
+        else {
+            cardPanel = new CardPanel(card.getSide2(), card.getSide1());
+        }
         
-        this.response = new ResponsePanel(str);
-        this.cardsides = new CardPanel (card);
+        ResponsePanel responsePanel = new ResponsePanel(response);
         
-        this.add(cardsides);
-        this.add(response);
-        this.validate();
+        JButton correctButton = new JButton("Correct");
+        JButton incorrectButton = new JButton("Incorrect");
+        
+        JPanel center = new JPanel();
+        center.setLayout(new GridLayout(1, 2));
+        
+        center.add(cardPanel);
+        center.add(responsePanel);
+        
+        JPanel south = new JPanel();
+        south.add(correctButton);
+        south.add(incorrectButton);
+        
+        
+        this.add(center, BorderLayout.CENTER);
+        this.add(south, BorderLayout.SOUTH);
+        
+        
+        
+    }
+    
+    class CardPanel extends JPanel {
+        String shownSide;
+        String reverseSide;
+        JLabel title;
+        JTextArea responseArea;
+        
+        CardPanel(String shownSide, String reverseSide) {
+            this.shownSide = shownSide;
+            this.reverseSide = reverseSide;
+            
+            //this.setLayout(new BorderLayout());
+            this.title = new JLabel("Shown Side");
+            JButton flip = new JButton("flip");
+            
+            flip.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(CardPanel.this.title.getText().equals("Shown Side")) {
+                        CardPanel.this.title.setText("Reverse Side");
+                        CardPanel.this.responseArea.setText(CardPanel.this.reverseSide);
+                        CardPanel.this.repaint();
+                    }
+                    else {
+                        CardPanel.this.title.setText("Shown Side");
+                        CardPanel.this.responseArea.setText(CardPanel.this.shownSide);
+                        CardPanel.this.repaint();
+                    }
+                }
+            });
+            
+            
+            
+            this.responseArea = new JTextArea(shownSide, 30, 40);
+            responseArea.setEnabled(false);
+            
+            
+            responseArea.setAlignmentX(JTextArea.CENTER_ALIGNMENT);
+            responseArea.setAlignmentY(JTextArea.CENTER_ALIGNMENT);
+            
+            responseArea.setBorder(new LineBorder(Color.BLACK, 1));
+            
+            JPanel north = new JPanel();
+            north.add(title);
+            north.add(flip);
+            north.setPreferredSize(new Dimension(500, 30));
+            
+            this.add(north, BorderLayout.NORTH);
+            this.add(responseArea, BorderLayout.CENTER);
+        }
+        
+    }
+    
+    class ResponsePanel extends JPanel {
+        String response;
+        
+        ResponsePanel(String response) {
+            this.response = response;
+            //this.setLayout(new BorderLayout());
+            JLabel title = new JLabel("Response");
+            JTextArea responseArea = new JTextArea(response, 30, 40);
+            responseArea.setEnabled(false);
+//            showText.setRows(10);
+//            showText.setMinimumSize(new Dimension(300, 300));
+            responseArea.setAlignmentX(JTextArea.CENTER_ALIGNMENT);
+            responseArea.setAlignmentY(JTextArea.CENTER_ALIGNMENT);
+            
+            responseArea.setBorder(new LineBorder(Color.BLACK, 1));
+            
+            JPanel north = new JPanel();
+            north.add(title);
+            north.setPreferredSize(new Dimension(500, 30));
+            
+            this.add(north, BorderLayout.NORTH);
+            this.add(responseArea, BorderLayout.CENTER);
+        }
     }
 } 
